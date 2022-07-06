60d976bd8e8119dfbde99a58a472ff551591832d
mohammedaminelahmami <lahmamimohammedamine@gmail.com>
2022-07-06 23:24:04 +0100

    first

---
+++ App.js
@@ -0,0 +1,18 @@
+// import { StatusBar } from 'expo-status-bar';
+import { Button, StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Hello World !</Text>
+      {/* <StatusBar style="auto" /> */}
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+});
